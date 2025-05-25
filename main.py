b9cf41353cb0ec66ea6d70aa61f61958197e4e0a
Yana <y.o.haievska@gmail.com>
2025-05-25 17:41:37 +0300

    Added 4th HW

--- main.py
+++ main.py
@@ -95,3 +95,51 @@
 # second_part = numbers[middle_numbers:]
 # result = [first_part, second_part]
 # print(result)
+
+#HW 4.1
+numbers = [4, 0, 0, 17, 23, 0, 8, 0, 5]
+non_zero_numbers = []
+zero_numbers = []
+
+for number in numbers:
+    if number != 0:
+        non_zero_numbers.append(number)
+    else:
+        zero_numbers.append(number)
+result_list = non_zero_numbers + zero_numbers
+print(result_list)
+
+#HW 4.2
+
+# numbers = []
+# sum_even = 0
+#
+# if numbers:
+#     last_number = numbers[-1]
+#     for i in range(0, len(numbers), 2):
+#         if i % 2 == 0:
+#             sum_even += numbers[i]
+#     result = sum_even * last_number
+#     print(result)
+# elif len(numbers) == 0:
+#     last_number = 0
+#     result = sum_even * last_number
+#     print(result)
+
+#v2
+numbers = []
+
+result = 0
+if numbers:
+    sum_even = 0
+    last_number = numbers[-1]
+    for i in range(len(numbers)):
+        if i % 2 == 0:
+            sum_even += numbers[i]
+    result = sum_even * last_number
+print(result)
+
+
+#HW 4.3
+numbers = [1, 2, 3, 4, 5, 6, 7, 9, 10]
+print(numbers[0], numbers[2], numbers[-2])
