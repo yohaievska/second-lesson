c101d653afc1c22b6cfa10a5a705bdae0a946c88
Yana <y.o.haievska@gmail.com>
2025-06-11 18:58:49 +0300

    Added 7th HW

--- main.py
+++ main.py
@@ -283,3 +283,49 @@ import string
 # assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
 # assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
 # print('ОК')
+
+#HW 7.2
+
+def correct_sentence(text):
+    text = text[0].upper() + text[1:]
+    if text[-1] != ".":
+        text += "."
+    return text
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
+
+#HW 7.3
+
+def second_index(text, some_str):
+    first = text.find(some_str)
+    if first == -1:
+        return None
+    second = text.find(some_str, first + len(some_str))
+    return second if second != -1 else None
+
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+print('ОК')
+
+#HW 7.4
+
+def common_elements():
+
+    nums = list()
+
+    for i in range(100):
+        # if i % 3 == 0 and i % 5 == 0:
+        if i % 3 == 0:
+            if i % 5 == 0:
+                nums.append(i)
+    return set(nums)
+
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+print('ОК')
